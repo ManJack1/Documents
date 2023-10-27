4b47f3f41d924d77b67171fe7a37331de625f386
ManJack <manjack0910@gamil.com>
2023-10-27 19:54:48 +0800

    My_WSLchange

--- c/c++/project_1/point.cpp
+++ c/c++/project_1/point.cpp
@@ -2,13 +2,33 @@
 
 using namespace std;
 
+const int MAX = 3;
+
 void main8() {
-  int var1;
-  char var2[10];
+  // NOTE: The point's Address
+  /* int var1; */
+  /* char var2[10]; */
+  /**/
+  /* cout << "Address of var1 variable: "; */
+  /* cout << &var1 << endl; */
+  /**/
+  /* cout << "Address of var2 variable: "; */
+  /* cout << &var2 << endl; */
 
-  cout << "Address of var1 variable: ";
-  cout << &var1 << endl;
+  // NOTE: The point's operation
 
-  cout << "Address of var2 variable: ";
-  cout << &var2 << endl;
+  int var[MAX] = {10, 100, 200};
+  int *ptr;
+
+  ptr = var;
+
+  for (int i = 0; i < MAX; i++) {
+    cout << "Address of var[" << i << "] = ";
+    cout << ptr << endl;
+
+    cout << "Value of var[" << i << "] = ";
+    cout << *ptr << endl;
+
+    ptr++;
+  }
 }
