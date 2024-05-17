9e9e83646fd7e97684d793beca71454b91c6e2e9
ManJack <manjack0910@gamil.com>
2024-05-17 14:41:15 +0800

    解决冲突后的描述

---
+++ c/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include "string"
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "The valuese 2,3 for two consecutive"
+       << "\n"
+       << "elements of a numerical sequence.\nwhat is the next value?\n";
+  return 0;
+}
